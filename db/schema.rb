c5860f3dca806e38fa13ea8d7b40c2af699e53e7
luisnlbs4@gmail.com <apple@apples-MacBook-Pro.local>
2019-03-19 22:42:05 -0400

    creando modelo de usuarios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_224601) do
+ActiveRecord::Schema.define(version: 2019_03_19_220154) do
 
   create_table "salas", force: :cascade do |t|
     t.string "nombre"
@@ -20,6 +20,20 @@ ActiveRecord::Schema.define(version: 2019_03_11_224601) do
     t.integer "minutos"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "nombre"
+    t.string "tipo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
